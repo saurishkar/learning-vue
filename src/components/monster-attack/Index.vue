d8196e293a69f654feeb55de9d1da3241a7850d9
Saurish Kar <basicallysaurish@gmail.com>
2020-06-01 08:17:17 +0530

    added event logging

--- src/components/monster-attack/Index.vue
+++ src/components/monster-attack/Index.vue
@@ -15,37 +15,45 @@
       <button class="start bg-primary">Start</button>
     </div>
     <div class="actions" v-if="started">
-      <button class="bg-red" @click="attack">Attack</button>
+      <button class="bg-red" @click="attackMonster">Attack</button>
       <button
         class="bg-yellow"
-        @click="specialAttack"
+        @click="attackMonsterSpecial"
         :disabled="specialAttackDisabled"
       >
         Special Attack
       </button>
-      <button class="bg-blue" @click="heal">Heal</button>
+      <button class="bg-blue" @click="healPlayer">Heal</button>
       <button class="bg-grey" @click="reset">Give up</button>
     </div>
+    <EventHistory :logs="eventHistory" v-if="started" />
   </div>
 </template>
 
 <script type="text/javascript">
 import Health from "./Health";
+import EventHistory from "./EventHistory";
+
+const FACTOR_20 = 20;
+const FACTOR_10 = 10;
+const MONSTER = "Monster";
+const PLAYER = "Player";
+const MONSTER_ATTACK_TIME = 500;
+const MAX_HEALTH = 100;
 
 export default {
   name: "Index",
   components: {
     Health,
+    EventHistory,
   },
   data() {
     return {
-      userHealth: 100,
-      monsterHealth: 100,
+      userHealth: MAX_HEALTH,
+      monsterHealth: MAX_HEALTH,
       started: false,
       monsterAttackTimeout: null,
-      getRandomFactor(max = 1) {
-        return Math.floor(Math.random() * Math.floor(max)) / 100;
-      },
+      eventHistory: [],
     };
   },
   computed: {
@@ -59,48 +67,70 @@ export default {
   methods: {
     start() {
       this.started = true;
-      this.monsterAttackTimeout = setTimeout(this.damageUserHealth, 500);
+      this.logEvent("Game Started");
+      this.attackPlayer();
     },
-    attack() {
-      this.damageMonsterHealth(10);
+    attackMonster() {
+      this.damage({ attacker: PLAYER });
     },
-    specialAttack() {
-      if (this.userHealth > 90) {
-        this.damageMonsterHealth(20);
+    attackMonsterSpecial() {
+      this.damage({ attacker: PLAYER, special: true });
+    },
+    attackPlayer() {
+      if (this.monsterAttackTimeout || !this.started) {
+        clearTimeout(this.monsterAttackTimeout);
+      }
+      if (this.started) {
+        this.monsterAttackTimeout = setTimeout(
+          () => this.damage({ attacker: MONSTER }),
+          MONSTER_ATTACK_TIME
+        );
       }
     },
-    heal() {
-      const newVal = this.userHealth * 1.1;
-      if (newVal > 100) {
-        this.userHealth = 100;
-      } else {
-        this.userHealth = newVal;
+    healPlayer() {
+      const healedValue = this.userHealth + FACTOR_10;
+      this.userHealth = healedValue >= MAX_HEALTH ? MAX_HEALTH : healedValue;
+      this.logEvent(`Player heals for ${FACTOR_10}`);
+    },
+    damagePlayerHealth(damageValue = 0) {
+      const newHealth = this.userHealth - damageValue;
+      this.logEvent(`Monster hits player for ${damageValue}`);
+      this.userHealth = newHealth <= 0 ? 0 : newHealth;
+      this.attackPlayer();
+    },
+    damageMonsterHealth(damageValue = 0, hard = false) {
+      const newHealth = this.monsterHealth - damageValue;
+      this.logEvent(
+        `Player hits monster ${hard ? "hard" : ""} for ${damageValue}`
+      );
+      this.monsterHealth = newHealth <= 0 ? 0 : newHealth;
+    },
+    damage({ attacker = MONSTER, special = false }) {
+      let damageValue = 0;
+      switch (attacker) {
+        case PLAYER:
+          damageValue = this.getRandomFactor(special ? FACTOR_20 : FACTOR_10);
+          this.damageMonsterHealth(damageValue, special);
+          break;
+
+        case MONSTER:
+          damageValue = this.getRandomFactor(FACTOR_20);
+          this.damagePlayerHealth(damageValue);
+          break;
       }
+    },
+    getRandomFactor(max = 1) {
+      return Math.floor(Math.random() * Math.floor(max));
+    },
+    logEvent(log = "") {
+      this.eventHistory = [log, ...this.eventHistory];
     },
     reset() {
-      this.userHealth = 100;
-      this.monsterHealth = 100;
+      this.userHealth = MAX_HEALTH;
+      this.monsterHealth = MAX_HEALTH;
       this.started = false;
+      this.eventHistory = [];
       clearTimeout(this.monsterAttackTimeout);
-    },
-    damageUserHealth() {
-      const newVal = this.userHealth * (1 - this.getRandomFactor(20));
-      if (newVal < 0) {
-        this.userHealth = 0;
-        clearTimeout(this.monsterAttackTimeout);
-      } else {
-        this.userHealth = newVal;
-        this.monsterAttackTimeout = setTimeout(this.damageUserHealth, 500);
-      }
-    },
-    damageMonsterHealth(maxPercent = 10) {
-      const newVal =
-        this.monsterHealth * (1 - this.getRandomFactor(maxPercent));
-      if (newVal < 0) {
-        this.monsterHealth = 0;
-      } else {
-        this.monsterHealth = newVal;
-      }
     },
   },
 };
